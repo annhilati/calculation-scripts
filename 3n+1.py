bc854a2db3332abb2d5adc0bbdb50ed0a1731cb9
Annhilati <78727320+annhilati@users.noreply.github.com>
2024-01-31 08:42:30 +0100

    Update 3n+1.py

--- 3n+1.py
+++ 3n+1.py
@@ -2,7 +2,7 @@ import time
 # Eingabe
 numberToBeginWith = int(input("Number to begin solving with for 3n+1: "))
 numbersToSolve = int(input("Numbers to solve for 3n+1: "))
-SolveTowardsMinus = True
+SolveTowardsMinus = False
 
 # Programm
 start_time = time.time()
@@ -38,5 +38,5 @@ print("--------[ RESULTS ]--------")
 print(f"Numbers solved:      {numberToBeginWith} to {numberToSolveFor}")
 print(f"Time needed:         {time.time() - start_time:.2f}s")
 print(f"Numbers per second:  {numbersToSolve / (time.time() - start_time):.2f}")
-
+print(" ")
 print(f"Number with most calculations needed: (see last line of the log)")

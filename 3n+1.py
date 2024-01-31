80a1bd53216221d8857157ae32e07d46be372e58
Annhilati <78727320+annhilati@users.noreply.github.com>
2024-01-31 08:52:25 +0100

    Update 3n+1.py

--- 3n+1.py
+++ 3n+1.py
@@ -2,7 +2,7 @@ import time
 # Eingabe
 numberToBeginWith = int(input("Number to begin solving with for 3n+1: "))
 numbersToSolve = int(input("Numbers to solve for 3n+1: "))
-SolveTowardsMinus = False
+SolveTowardsMinus = True
 
 # Programm
 start_time = time.time()
@@ -17,15 +17,16 @@ while numbersStillToSolve > 0: # and numberToSolveFor >= numberToBeginWith:
     while numberCalc != 1 and numberCalc != 0 and numberCalc != -1 and numberCalc != -5 and numberCalc != -17:
         if numberCalc % 2 == 1:
             numberCalc = numberCalc * 3 + 1
-            operationsCalc = operationsCalc + 1
         else:
             numberCalc = numberCalc / 2
-            operationsCalc = operationsCalc + 1
-    # Schreibt neue Rekorde in die Konsole        
-    if operationsCalc > operationsRecord:
-        print(f"{time.time() - start_time:.2f}s: Number {numberToSolveFor} broke the record of {operationsRecord} operations and is now {operationsCalc}, {operationsDone} total operations so far")
-        operationsRecord = operationsCalc
+        operationsCalc = operationsCalc + 1
+            
+    # Schreibt neue Rekorde in die Konsole
     operationsDone = operationsDone + operationsCalc
+    if operationsCalc > operationsRecord:
+        print(f"{time.time() - start_time:.2f}s: Number {numberToSolveFor} broke the record of {operationsRecord} operations which is now {operationsCalc}, {operationsDone} total operations so far")
+        operationsRecord = operationsCalc
+    
     # Gibt an, welche Zahl als nächstes gelöst wird
     numbersStillToSolve = numbersStillToSolve - 1
     if SolveTowardsMinus == False:

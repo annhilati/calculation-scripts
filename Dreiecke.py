be0bddc81513a80fec136e3da7167025ef460f12
Annhilati <78727320+annhilati@users.noreply.github.com>
2024-05-16 19:00:29 +0200

    Update Dreiecke.py

--- Dreiecke.py
+++ Dreiecke.py
@@ -18,64 +18,75 @@ def v(sideVector):
 def degree(radian):
     return radian * (180 / pi)
 
-print("╔════════════════════════════════════════════════════════════════════╗")
-print("║ Geben Sie Daten für ein Dreieck an                                 ║")
-print("║ Dieser Rechner verwendet A B C a b c. Bitte bedenken Sie dies      ║")
-print("╠════════════════════════════════════════════════════════════════════╣")
-print("║ Geben Sie die Seitenlänge von a an.                                ║")
-print("╚═╦══════════════════════════════════════════════════════════════════╝")
-sideA = inputRealNotZero("  ║ a <= ")
-print("╔═╩══════════════════════════════════════════════════════════════════╗")
-print("║ Geben Sie die Seitenlänge von b an.                                ║")
-print("╚═╦══════════════════════════════════════════════════════════════════╝")
-sideB = inputRealNotZero("  ║ b <= ")
-print("╔═╩══════════════════════════════════════════════════════════════════╗")
-print("║ Geben Sie die Seitenlänge von c an.                                ║")
-print("╚═╦══════════════════════════════════════════════════════════════════╝")
-sideC = inputRealNotZero("  ║ c <= ")
+while True:
+    print("╔════════════════════════════════════════════════════════════════════╗")
+    print("║ Geben Sie Daten für ein Dreieck an                                 ║")
+    print("║ Dieser Rechner verwendet A B C a b c. Bitte bedenken Sie dies      ║")
+    print("╠════════════════════════════════════════════════════════════════════╣")
+    print("║ Geben Sie die Seitenlänge von a an.                                ║")
+    print("╚═╦══════════════════════════════════════════════════════════════════╝")
+    sideA = inputRealNotZero("  ║ a <= ")
+    print("╔═╩══════════════════════════════════════════════════════════════════╗")
+    print("║ Geben Sie die Seitenlänge von b an.                                ║")
+    print("╚═╦══════════════════════════════════════════════════════════════════╝")
+    sideB = inputRealNotZero("  ║ b <= ")
+    print("╔═╩══════════════════════════════════════════════════════════════════╗")
+    print("║ Geben Sie die Seitenlänge von c an.                                ║")
+    print("╚═╦══════════════════════════════════════════════════════════════════╝")
+    sideC = inputRealNotZero("  ║ c <= ")
 
-sidesListed = [["a", sideA], ["b", sideB], ["c", sideC]]
-sides = sorted(sidesListed, key=lambda x: (x[1], x[0]))
-#print("  ║ [DEBUG]", sides) # Debug
+    sidesListed = [["a", sideA], ["b", sideB], ["c", sideC]]
+    sides = sorted(sidesListed, key=lambda x: (x[1], x[0]))
+    #print("  ║ [DEBUG]", sides) # Debug
 
-print(f"╔═╩══════════════════════════════════════════════════════════════════╗")
-print(f"║ Daten werden geprüft...                                             ")
-print(f"║   a = {sideA}")
-print(f"║   b = {sideB}")
-print(f"║   c = {sideC}")
-print(f"╚═╦══════════════════════════════════════════════════════════════════╝")
+    print(f"╔═╩══════════════════════════════════════════════════════════════════╗")
+    print(f"║ Daten werden geprüft...                                             ")
+    print(f"║   a = {sideA}")
+    print(f"║   b = {sideB}")
+    print(f"║   c = {sideC}")
+    print(f"╚═╦══════════════════════════════════════════════════════════════════╝")
 
-if not v(sides[2]) <= v(sides[0]) + v(sides[1]):
-    print("  ║ [COMMENT] Die Seitenlängen ergeben kein gültiges Dreieck")
-else:
-    print("  ║ [COMMENT] Die Seiten ergeben ein gültiges Dreieck")
-    print("  ║ ")
+    if not v(sides[2]) <= v(sides[0]) + v(sides[1]):
+        print("  ║ [COMMENT] Die Seitenlängen ergeben kein gültiges Dreieck")
+    else:
+        print("  ║ [COMMENT] Die Seiten ergeben ein gültiges Dreieck")
+        print("  ║ ")
 
-    # Fläche
-    s = (sideA + sideB + sideC) / 2
-    area = sqrt(s * (s - sideA) * (s - sideB) * (s - sideC))
-    print(f"  ║ Fläche ≈ {area:.2f}")
+        # Fläche
+        s = (sideA + sideB + sideC) / 2
+        area = sqrt(s * (s - sideA) * (s - sideB) * (s - sideC))
+        print(f"  ║ Fläche ≈ {area:.2f}")
 
-    # Höhen
-    heightA = (area * 2) / sideA
-    heightB = (area * 2) / sideB
-    heightC = (area * 2) / sideC
-    print(f"  ║ Höhe auf a ≈ {heightA:.2f}")
-    print(f"  ║ Höhe auf b ≈ {heightB:.2f}")
-    print(f"  ║ Höhe auf c ≈ {heightC:.2f}")
-    print(f"  ║")
+        # Höhen
+        heightA = (area * 2) / sideA
+        heightB = (area * 2) / sideB
+        heightC = (area * 2) / sideC
+        print(f"  ║ Höhe auf a ≈ {heightA:.2f}")
+        print(f"  ║ Höhe auf b ≈ {heightB:.2f}")
+        print(f"  ║ Höhe auf c ≈ {heightC:.2f}")
+        print(f"  ║")
 
-    # Innenwinkel
-    angleAlpha = degree(acos((sideB ** 2 + sideC ** 2 - sideA ** 2) / (2 * sideB * sideC)))
-    angleBeta = degree(acos((sideA ** 2 + sideC ** 2 - sideB ** 2) / (2 * sideA * sideC)))
-    angleGamma = degree(acos((sideA ** 2 + sideB ** 2 - sideC ** 2) / (2 * sideA * sideB)))
-    print(f"  ║ α = ∠A ≈ {angleAlpha:.2f}°")
-    print(f"  ║ β = ∠B ≈ {angleBeta:.2f}°")
-    print(f"  ║ γ = ∠C ≈ {angleGamma:.2f}°")
+        # Innenwinkel
+        angleAlpha = degree(acos((sideB ** 2 + sideC ** 2 - sideA ** 2) / (2 * sideB * sideC)))
+        angleBeta = degree(acos((sideA ** 2 + sideC ** 2 - sideB ** 2) / (2 * sideA * sideC)))
+        angleGamma = degree(acos((sideA ** 2 + sideB ** 2 - sideC ** 2) / (2 * sideA * sideB)))
+        print(f"  ║ α = ∠A ≈ {angleAlpha:.3f}°")
+        print(f"  ║ β = ∠B ≈ {angleBeta:.3f}°")
+        print(f"  ║ γ = ∠C ≈ {angleGamma:.3f}°")
+        print(f"  ║")
 
-    # Rechtwinkeleigenschaften
-    if v(sides[2]) ** 2 == v(sides[0]) ** 2 + v(sides[1]) ** 2:
-        print(f"  ║ [COMMENT] Das Dreieck ist rechtwinklig")
-        print(f"  ║ Die Hypothenuse ist {sides[2][0]}")
-    else: 
-        print(f"  ║ [COMMENT] Das Dreieck hat keinen rechten Winkel")
+        # Dreieckstyp
+        if v(sides[0]) == v(sides[1]) and v(sides[2]):
+            print(f"  ║ Typ: gleichseitig")
+        elif v(sides[1]) == v(sides[2]):
+            print(f"  ║ Typ: gleichschenklig")
+            print(f"  ║ {sides[1][0]} und {sides[2][0]} sind gleich")
+        else: 
+            print(f"  ║ Typ: unregelmäßig")
+        print(f"  ║")
+        if v(sides[2]) ** 2 == v(sides[0]) ** 2 + v(sides[1]) ** 2:
+            print(f"  ║ Das Dreieck ist rechtwinklig")
+            print(f"  ║ Die Hypothenuse ist {sides[2][0]}")
+
+    print("  ╚══════════════════════════════════════════════════════════════════╝")
+    input()
